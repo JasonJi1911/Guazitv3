412fff1ddf7c8973eda09d23d9d85aa84e264a6e
yin-mn <1802903057@qq,com>
2022-06-21 15:59:06 +0800

    page->CollectController.php

--- page/controllers/CollectController.php
+++ page/controllers/CollectController.php
@@ -53,31 +53,32 @@ class CollectController extends Controller
         if($res['code']>1){
             return '没有数据';
         }
-
+        echo(intval($res['page']['pagecount']));
         for ($i = 1; $i<=intval($res['page']['pagecount']);$i++)
         {
             $param['page'] = $i;
             if ($param['mid'] == '' || $param['mid'] == '1') {
                 $this->vod($param);
+                echo($i);
             }
         }
     }
 
     private function vod($param)
     {
-        if($param['ac'] != 'list'){
-            $redis = new RedisStore();
-            $catchKey = 'collect_break_vod_'.$param['collect_id'].'_'.$param['h'];
-//            $catch = Url::to('collect/api', $param);
-            $catch = $redis->get($catchKey);
-            if (isset($catch) && !isset($param['page']))
-            {
-                $temp = json_decode($catch, true);
-                $param = $temp;
-            }else{
-                $redis->setEx($catchKey, json_encode($param, JSON_UNESCAPED_UNICODE));
-            }
-        }
+//         if($param['ac'] != 'list'){
+//             $redis = new RedisStore();
+//             $catchKey = 'collect_break_vod_'.$param['collect_id'].'_'.$param['h'];
+// //            $catch = Url::to('collect/api', $param);
+//             $catch = $redis->get($catchKey);
+//             if (isset($catch) && !isset($param['page']))
+//             {
+//                 $temp = json_decode($catch, true);
+//                 $param = $temp;
+//             }else{
+//                 $redis->setEx($catchKey, json_encode($param, JSON_UNESCAPED_UNICODE));
+//             }
+//         }
         $collectModel = new Collect();
         $res = $collectModel->vod($param);
         if($res['code']>1){
@@ -130,7 +131,7 @@ class CollectController extends Controller
 
             $proxyResult = Tool::httpGetProxy($adUrl,  $proxyData->data[0]->ip, $proxyData->data[0]->port);
             echo($i);
-            // sleep(5);
+            sleep(1);
         }
     }
 
@@ -146,7 +147,7 @@ class CollectController extends Controller
 
         $mysql_conf = array(
             'user' => 'beiwo2',
-            'password' => 'CRLmRnCrjXSbfGxS',
+            'password' => 'WLP274fMZrJJ8r6j',
             'port' => 3306,
             'host' => '127.0.0.1',
             'db' => 'beiwo2',
@@ -172,8 +173,7 @@ class CollectController extends Controller
             $ifvodItem['vod_play_url'] = $res['vod_play_url'];
             $ifvodItem['vod_play_url'] = $ifvodItem['chapter_name']. "$" . explode("$", $res['vod_play_url'])[1];
             $collectModel = new Collect();
-//            $param['source'] = 1;
-            $param['source'] = $ifvodItem['source'];
+            $param['source'] = 1;
             $result = $collectModel->vod_ifvoddata($param, $ifvodItem);
         }
 
@@ -187,11 +187,82 @@ class CollectController extends Controller
             $ifvodItem['vod_play_url'] = $res['vod_play_url'];
             $ifvodItem['vod_play_url'] = $ifvodItem['chapter_name']. "$" . explode("$", $res['vod_play_url'])[1];
             $collectModel = new Collect();
-//            $param['source'] = 10;
-            $param['source'] = $ifvodItem['source'];
+            $param['source'] = 10;
+            $result = $collectModel->vod_ifvoddata($param, $ifvodItem);
+        }
+
+        $sql = "SELECT * FROM vod_Play_480 where final_title=".$res['vod_key'];
+        $db_result = $conn->query($sql);
+        $ifvodItem = $db_result->fetch_assoc();
+        if($ifvodItem)
+        {
+            $upSql = "UPDATE vod_Play_480 set dir='".$res['outdir']."' where final_title=".$res['vod_key'];
+            $ss = $conn->query($upSql);
+            $ifvodItem['vod_play_url'] = $res['vod_play_url'];
+            $ifvodItem['vod_play_url'] = $ifvodItem['chapter_name']. "$" . explode("$", $res['vod_play_url'])[1];
+            $collectModel = new Collect();
+            $param['source'] = 20;
             $result = $collectModel->vod_ifvoddata($param, $ifvodItem);
         }
         $result['ss'] = $ss;
+
+        // $conn->close();
+        echo json_encode($result, JSON_UNESCAPED_UNICODE);
+//        return Tool::responseJson(0, '操作成功', $param);
+    }
+
+    public function actionPushSong()
+    {
+        $res['vod_key'] = Yii::$app->request->post('vod_key', '');
+        $res['vod_name'] = Yii::$app->request->post('vod_name', '');
+        $res['vod_en'] = Yii::$app->request->post('vod_en', '');
+        $res['vod_letter'] = Yii::$app->request->post('vod_letter', '');
+        $res['vod_chapter'] = Yii::$app->request->post('vod_chapter', '');
+        $res['vod_play_url'] = Yii::$app->request->post('vod_play_url', '');
+        $res['outdir'] = Yii::$app->request->post('outdir', '');
+        $quality = Yii::$app->request->post('quality', '');
+
+        $mysql_conf = array(
+            'user' => 'beiwo2',
+            'password' => 'WLP274fMZrJJ8r6j',
+            'port' => 3306,
+            'host' => '127.0.0.1',
+            'db' => 'beiwo2',
+            'charset' => 'utf8'
+        );
+
+        $conn = new mysqli($mysql_conf['host'], $mysql_conf['user'], $mysql_conf['password'], $mysql_conf['db'], $mysql_conf['port']);
+        // 检测连接
+        if ($conn->connect_error) {
+            echo("连接失败: " . $conn->connect_error);
+        }
+
+        // $area_list = $conn->query("SELECT * FROM sf_video_area")->fetch_assoc();
+
+        $param['isdownload'] = Collect::COLLECT_DOWNLOAD_NO;
+        $sql = "SELECT * FROM vod_Play_720 where final_title=".$res['vod_key'];
+        $db_result = $conn->query($sql);
+        $ifvodItem = $db_result->fetch_assoc();
+        if($ifvodItem && $quality == '720P')
+        {
+            $upSql = "UPDATE vod_Play_720 set dir='".$res['outdir']."' where final_title=".$res['vod_key'];
+            $ss = $conn->query($upSql);
+            $ifvodItem['vod_play_url'] = $res['vod_play_url'];
+            $ifvodItem['vod_play_url'] = $ifvodItem['chapter_name']. "$" . explode("$", $res['vod_play_url'])[1];
+            $collectModel = new Collect();
+            $param['source'] = 1;
+            $result = $collectModel->vod_ifvoddata($param, $ifvodItem);
+        }
+        elseif($ifvodItem && $quality == '480P')
+        {
+            $ifvodItem['vod_play_url'] = $res['vod_play_url'];
+            $ifvodItem['vod_play_url'] = $ifvodItem['chapter_name']. "$" . explode("$", $res['vod_play_url'])[1];
+            $collectModel1 = new Collect();
+            $param['source'] = 20;
+            $result = $collectModel1->vod_ifvoddata($param, $ifvodItem);
+        }
+        $result['ss'] = $ss;
+        $result['sk'] = $ifvodItem;
 
         // $conn->close();
         echo json_encode($result, JSON_UNESCAPED_UNICODE);
